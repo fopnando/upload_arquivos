33c6b0211f6babaf14d46e96d1cef4fa038a6a22
Fernando.pina <fernando.pina@7comm.com.br>
2020-10-15 22:00:43 -0300

    first commit , subindo projeto upload de arquivos

---
+++ src/app/upload-file/upload-file/upload-file.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { UploadFileService } from '../upload-file.service';
+import { environment } from '../../../environments/environment';
+import { filterResponse, uploadProgress } from '../../shared/rxjs-operators';
+
+@Component({
+  selector: 'app-upload-file',
+  templateUrl: './upload-file.component.html',
+  styleUrls: ['./upload-file.component.scss']
+})
+export class UploadFileComponent implements OnInit {
+
+  files: Set<File>;
+  progress = 0;
+
+  constructor(private service: UploadFileService) { }
+
+  ngOnInit() { }
+
+  onChange(event) {
+    console.log(event);
+
+    const selectedFiles = <FileList>event.srcElement.files;
+    // document.getElementById('customFileLabel').innerHTML = selectedFiles[0].name;
+
+    const fileNames = [];
+    this.files = new Set();
+    for (let i = 0; i < selectedFiles.length; i++) {
+      fileNames.push(selectedFiles[i].name);
+      this.files.add(selectedFiles[i]);
+    }
+    document.getElementById('customFileLabel').innerHTML = fileNames.join(', ');
+
+    this.progress = 0;
+  }
+
+  onUpload() {
+    if (this.files && this.files.size > 0) {
+      this.service.upload(this.files, environment.BASE_URL + '/upload')
+        .pipe(
+          uploadProgress(progress => {
+            console.log(progress);
+            this.progress = progress;
+          }),
+          filterResponse()
+        )
+        .subscribe(response => console.log('Upload Concluído'));
+      
+    }
+  }
+ 
+}
